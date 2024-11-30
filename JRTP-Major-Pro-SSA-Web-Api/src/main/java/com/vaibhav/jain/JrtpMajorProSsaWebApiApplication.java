183654e11e37b8387b74c9eabc7388c6fdb0a234
Vaibhav Jain <vaibhavjain8880@gmail.com>
2024-11-30 13:43:11 +0530

    Extranal RestService-SSAWeb

---
+++ JRTP-Major-Pro-SSA-Web-Api/src/main/java/com/vaibhav/jain/JrtpMajorProSsaWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.jain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrtpMajorProSsaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrtpMajorProSsaWebApiApplication.class, args);
+	}
+
+}
